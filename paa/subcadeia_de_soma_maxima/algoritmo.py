8992acf39c0555248cbc093cbe4781d2c095d940
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-03-09 00:15:05 -0300

    Melhora algoritmo para O(n^2)

--- paa/subcadeia_de_soma_maxima/algoritmo.py
+++ paa/subcadeia_de_soma_maxima/algoritmo.py
@@ -1,15 +1,18 @@
 #encoding: utf-8
 
 def subcadeia_de_soma_maxima(cadeia):
-  inicio_intervalo_soma_maxima, fim_intervalo_soma_maxima, soma  = 0, 0, 0
+  inicio_intervalo_soma_maxima, fim_intervalo_soma_maxima, total  = 0, 0, 0
   indices_cadeia = range(len(cadeia))
 
   for inicio in indices_cadeia:
+    soma = cadeia[inicio]
     for fim in indices_cadeia[inicio:]:
+      if inicio != fim:
+        soma += cadeia[fim]
 
-      if sum(cadeia[inicio:fim+1]) > soma:
-        soma = sum(cadeia[inicio:fim+1])
+      if soma > total:
+        total = soma
         inicio_intervalo_soma_maxima = inicio
         fim_intervalo_soma_maxima = fim
 
-  return ([inicio_intervalo_soma_maxima, fim_intervalo_soma_maxima], soma)
+  return ([inicio_intervalo_soma_maxima, fim_intervalo_soma_maxima], total)
