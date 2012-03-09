4d34c8bd58c9f5a0518e49109ccd713a7c0e91f1
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-03-09 00:39:16 -0300

    Melhora algoritmo para O(n)

--- paa/subcadeia_de_soma_maxima/algoritmo.py
+++ paa/subcadeia_de_soma_maxima/algoritmo.py
@@ -3,16 +3,18 @@
 def subcadeia_de_soma_maxima(cadeia):
   inicio_intervalo_soma_maxima, fim_intervalo_soma_maxima, total  = 0, 0, 0
   indices_cadeia = range(len(cadeia))
+  inicio, soma = 0, 0
 
-  for inicio in indices_cadeia:
-    soma = cadeia[inicio]
-    for fim in indices_cadeia[inicio:]:
-      if inicio != fim:
-        soma += cadeia[fim]
+  for fim in indices_cadeia:
+    soma += cadeia[fim]
 
-      if soma > total:
-        total = soma
-        inicio_intervalo_soma_maxima = inicio
-        fim_intervalo_soma_maxima = fim
+    if soma < 0:
+      inicio = fim+1
+      soma = 0
+
+    if soma > total:
+      total = soma
+      inicio_intervalo_soma_maxima = inicio
+      fim_intervalo_soma_maxima = fim
 
   return ([inicio_intervalo_soma_maxima, fim_intervalo_soma_maxima], total)
--- paa/subcadeia_de_soma_maxima/testes.py
+++ paa/subcadeia_de_soma_maxima/testes.py
@@ -7,3 +7,6 @@ def test_subcadeia_de_soma_maxima_deve_retornar_posicoes_inicial_e_final_e_soma(
 def test_subcadeia_de_soma_maxima_deve_buscar_pela_subcadeia_de_maior_soma():
   cadeia = [-2, 11, -4, 13, -5, -2] #exemplo da sala de aula
   assert ([1, 3], 20) == subcadeia_de_soma_maxima(cadeia) #indices diferentes do slide pq arrays em python comecam de 0
+
+  cadeia2 = [1000, -10001, 1, 2, 3]
+  assert ([0,0], 1000) == subcadeia_de_soma_maxima(cadeia2)
