5e1cf5e2f9fb9726268fb19e9f8c15c3454e0460
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-03-11 16:40:34 -0300

    Adiciona NodesFactory que linka os nós em arquitetura de anel

--- algoritmos_distribuidos/anel_chang_roberts/algoritmo.py
+++ algoritmos_distribuidos/anel_chang_roberts/algoritmo.py
@@ -1,11 +1,28 @@
+#encoding: utf-8
 
 import uuid
 
+class NodesFactory(object):
+    def build_nodes(self, qty):
+        first_node = Node()
+        previous_node = first_node
+        nodes = [previous_node]
+
+        for i in range(qty-1):
+            new_node = Node()
+            previous_node.next = new_node
+            previous_node = new_node
+            nodes.append(previous_node)
+
+        new_node.next = first_node
+
+        return nodes
+
 class Node(object):
     def __init__(self):
-        self.pid = self._cria_pid()
-        self.next = self
+        self.pid = self._build_pid()
+        self.next = self # se apenas um nó for criado, ele é linkado nele mesmo
 
-    def _cria_pid(self):
+    def _build_pid(self):
         return uuid.uuid4().int
 
--- algoritmos_distribuidos/anel_chang_roberts/testes.py
+++ algoritmos_distribuidos/anel_chang_roberts/testes.py
@@ -1,6 +1,6 @@
 #encoding: utf-8
 
-from algoritmo import Node
+from algoritmo import Node, NodesFactory
 
 def test_node_precisa_ter_pid_e_link_para_proximo_processo():
     node1 = Node()
@@ -15,4 +15,12 @@ def test_nodes_precisam_ter_pids_unicos():
 
     assert not [pid for pid in pids if pids.count(pid) > 1]
 
+def test_links_dos_nodes_devem_criar_um_anel():
+    nodes_factory = NodesFactory()
+    nodes = nodes_factory.build_nodes(3)
+
+    assert len(nodes) == 3
+    assert nodes[0].next == nodes[1]
+    assert nodes[1].next == nodes[2]
+    assert nodes[2].next == nodes[0]
 
