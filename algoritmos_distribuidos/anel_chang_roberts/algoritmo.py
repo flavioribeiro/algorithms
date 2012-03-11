3832b16186b758058ea8d1e2205adcd85a3e461f
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-03-11 16:00:30 -0300

    Adiciona PID unico pros nós

--- algoritmos_distribuidos/anel_chang_roberts/algoritmo.py
+++ algoritmos_distribuidos/anel_chang_roberts/algoritmo.py
@@ -1,9 +1,11 @@
 
+import uuid
+
 class Node(object):
     def __init__(self):
         self.pid = self._cria_pid()
         self.next = self
 
     def _cria_pid(self):
-        return 1
+        return uuid.uuid4().int
 
--- algoritmos_distribuidos/anel_chang_roberts/testes.py
+++ algoritmos_distribuidos/anel_chang_roberts/testes.py
@@ -2,10 +2,17 @@
 
 from algoritmo import Node
 
-def test_node_precisa_ter_pid_e_proximo_processo():
+def test_node_precisa_ter_pid_e_link_para_proximo_processo():
     node1 = Node()
-    assert type(node1.pid) == int
+    assert type(node1.pid) == long
     assert type(node1.next) == Node
 
+def test_nodes_precisam_ter_pids_unicos():
+    pids = []
+    for i in range(1000):
+        node = Node()
+        pids.append(node.pid)
+
+    assert not [pid for pid in pids if pids.count(pid) > 1]
 
 
