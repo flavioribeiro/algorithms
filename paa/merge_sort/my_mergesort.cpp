86701e5bdbad6d624ac1980ee9b1bff2ff7e1371
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-03-24 17:49:51 -0300

    Retira o 'big_int'

--- paa/merge_sort/my_mergesort.cpp
+++ paa/merge_sort/my_mergesort.cpp
@@ -24,6 +24,7 @@ vector<int> my_mergesort(vector<int> input) {
         output = combine(ordered_left_subarray, ordered_right_subarray);
 
         return output;
+
     } else {
         return input;
     }
@@ -33,23 +34,21 @@ vector<int> combine(vector<int> left_subarray, vector<int> right_subarray) {
     int left_subarray_position = 0, right_subarray_position = 0;
     vector<int> output;
     int size_of_arrays = left_subarray.size();
-    int big_int = 1000000000;
-
-    left_subarray.push_back(big_int);
-    right_subarray.push_back(big_int);
 
     for (int i=0; i <= (size_of_arrays) * 2; i++) {
 
-        if (left_subarray[left_subarray_position] == big_int && right_subarray[right_subarray_position] == big_int) {
-            //do nothing :P
-        } else if (left_subarray[left_subarray_position] < right_subarray[right_subarray_position]) {
-            output.push_back(left_subarray[left_subarray_position]);
-            left_subarray_position++;
-
+        if (left_subarray_position == size_of_arrays && right_subarray_position == size_of_arrays) {
+            // do nothing :P
+        } else if (left_subarray_position == size_of_arrays && right_subarray_position != size_of_arrays) {
+            output.push_back(right_subarray[right_subarray_position++]);
+        } else if (left_subarray_position != size_of_arrays && right_subarray_position == size_of_arrays) {
+            output.push_back(left_subarray[left_subarray_position++]);
+         } else if (left_subarray[left_subarray_position] < right_subarray[right_subarray_position]) {
+            output.push_back(left_subarray[left_subarray_position++]);
         } else {
-            output.push_back(right_subarray[right_subarray_position]);
-            right_subarray_position++;
+            output.push_back(right_subarray[right_subarray_position++]);
         }
     }
+
     return output;
 }
