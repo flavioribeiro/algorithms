0a3d3f1669ffdaacfc8259861f77157c4bceee00
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-04-23 19:46:28 -0300

    Adiciona multiplicacao de inteiros grandes para numeros maiores que max_int

--- paa/mult_inteiros_grandes/tests.py
+++ paa/mult_inteiros_grandes/tests.py
@@ -5,3 +5,10 @@ def test_mult_inteiros_grandes_para_inteiros_menores_que_max_int():
     n_2 = 1130
 
     assert 2374130 == mult(n_1, n_2)
+
+def test_mult_inteiros_grandes_para_inteiros_maiores_que_max_int():
+    n_1 = 10242204
+    n_2 = 10002400
+    result = mult(n_1, n_2)
+
+    assert 102446621289600 == mult(n_1, n_2)
