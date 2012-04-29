02c5967d8da7c0698baf3f07b8c7af3b72fcdcca
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-04-29 00:37:58 -0300

    Adiciona mais logging e asserts

--- paa/randomized_select/randomized_select.py
+++ paa/randomized_select/randomized_select.py
@@ -1,10 +1,12 @@
+#encoding: utf-8
 import random
 
 def RANDOMIZED_PARTITION(vetor, p, r):
     ultima_posicao = r-1
-    print "Randomized Partition comecou:",  vetor
-#    pivot = vetor[random.randrange(p, r-1)]
+    print "- Randomized Partition comecou:",  vetor, "Verificando de ", p,"ate", r
     pivot = vetor[ultima_posicao]
+#    pivot = random.randrange(p, r)
+
     print "- Pivot escolhido", pivot
     i = p
     for j in range(p, ultima_posicao):
@@ -12,18 +14,24 @@ def RANDOMIZED_PARTITION(vetor, p, r):
             vetor[i], vetor[j] = vetor[j], vetor[i]
             i += 1
     vetor[i], vetor[ultima_posicao] = vetor[ultima_posicao], vetor[i]
-    print "Randomized Partition terminou:", vetor
+    print "- Randomized Partition terminou:", vetor, " Retornando:", i
     return i
 
 def RANDOMIZED_SELECT(vetor, posicao_inicial, posicao_final, i):
+    print "RANDOMIZED_SELECT", vetor, posicao_inicial, posicao_final, i
     if posicao_final + 1 == posicao_inicial:
         return vetor[posicao_inicial]
     q = RANDOMIZED_PARTITION(vetor, posicao_inicial, posicao_final)
     k = q - posicao_inicial
     if i == k:
+        print "O escolhido é o pivot"
         return vetor[q]
     elif i < k:
+        print "[i %d < k %d]" %(i,k), vetor, posicao_inicial, q, i
         return RANDOMIZED_SELECT(vetor, posicao_inicial, q, i)
-    return RANDOMIZED_SELECT(vetor, q, posicao_final, i - k)
+
+    else:
+        print "[i %d > k %d]" %(i,k), vetor, q, posicao_final, i-k
+        return RANDOMIZED_SELECT(vetor, q, posicao_final, i - k)
 
 
--- paa/randomized_select/tests.py
+++ paa/randomized_select/tests.py
@@ -16,4 +16,6 @@ def test_select_median():
 def test_select_terceiro_menor():
     data = [20, 10, 12, 30, 16, 100, 70]
 
+    assert 30 == RANDOMIZED_SELECT(data, 0, len(data), 4)
+    assert 10 == RANDOMIZED_SELECT(data, 0, len(data), 0)
     assert 16 == RANDOMIZED_SELECT(data, 0, len(data), 2)
