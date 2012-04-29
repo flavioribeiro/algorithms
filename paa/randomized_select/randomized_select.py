077fecdbbd73ca2dd35cf054a2ae0198148d2bbd
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-04-28 23:41:08 -0300

    Adiciona mais um teste no randomized_select.py e adiciona comentários

--- paa/randomized_select/randomized_select.py
+++ paa/randomized_select/randomized_select.py
@@ -1,22 +1,28 @@
-def RANDOMIZED_PARTITION(A, p, r):
-    x = A[r-1]
+import random
+
+def RANDOMIZED_PARTITION(vetor, p, r):
+    print "Randomized Partition comecou:",  vetor
+#    pivot = vetor[random.randrange(p, r-1)]
+    pivot = vetor[r-1]
+    print "- Pivot escolhido", pivot
     i = p
     for j in range(p, r-1):
-        if A[j] <= x:
-            A[i], A[j] = A[j], A[i]
+        if vetor[j] <= pivot:
+            vetor[i], vetor[j] = vetor[j], vetor[i]
             i += 1
-    A[i], A[r-1] = A[r-1], A[i]
+    vetor[i], vetor[r-1] = vetor[r-1], vetor[i]
+    print "Randomized Partition terminou:", vetor
     return i
 
-def RANDOMIZED_SELECT(A, p, r, i):
-    if p + 1 == r:
-        return A[p]
-    q = RANDOMIZED_PARTITION(A, p, r)
-    k = q - p
+def RANDOMIZED_SELECT(vetor, posicao_inicial, posicao_final, i):
+    if posicao_final + 1 == posicao_inicial:
+        return vetor[posicao_inicial]
+    q = RANDOMIZED_PARTITION(vetor, posicao_inicial, posicao_final)
+    k = q - posicao_inicial
     if i == k:
-        return A[q]
+        return vetor[q]
     elif i < k:
-        return RANDOMIZED_SELECT(A, p, q, i)
-    return RANDOMIZED_SELECT(A, q, r, i - k)
+        return RANDOMIZED_SELECT(vetor, posicao_inicial, q, i)
+    return RANDOMIZED_SELECT(vetor, q, posicao_final, i - k)
 
 
--- paa/randomized_select/tests.py
+++ paa/randomized_select/tests.py
@@ -12,3 +12,8 @@ def test_select_median():
     data = [1, 7, 3, 5, 0, 9, 4, 6, 2, 8]
     assert 4 == RANDOMIZED_SELECT(data, 0, len(data), 4)
     assert 5 == RANDOMIZED_SELECT(data, 0, len(data), 5)
+
+def test_select_terceiro_menor():
+    data = [20, 10, 12, 30, 16, 100, 70]
+
+    assert 16 == RANDOMIZED_SELECT(data, 0, len(data), 2)
