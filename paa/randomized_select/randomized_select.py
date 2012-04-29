d16a8ea701de04ae136071ecfeabf6812a03605d
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-04-28 23:50:51 -0300

    Altera nome de variáveis no RANDOMIZED_PARTITION

--- paa/randomized_select/randomized_select.py
+++ paa/randomized_select/randomized_select.py
@@ -1,16 +1,17 @@
 import random
 
 def RANDOMIZED_PARTITION(vetor, p, r):
+    ultima_posicao = r-1
     print "Randomized Partition comecou:",  vetor
 #    pivot = vetor[random.randrange(p, r-1)]
-    pivot = vetor[r-1]
+    pivot = vetor[ultima_posicao]
     print "- Pivot escolhido", pivot
     i = p
-    for j in range(p, r-1):
+    for j in range(p, ultima_posicao):
         if vetor[j] <= pivot:
             vetor[i], vetor[j] = vetor[j], vetor[i]
             i += 1
-    vetor[i], vetor[r-1] = vetor[r-1], vetor[i]
+    vetor[i], vetor[ultima_posicao] = vetor[ultima_posicao], vetor[i]
     print "Randomized Partition terminou:", vetor
     return i
 
