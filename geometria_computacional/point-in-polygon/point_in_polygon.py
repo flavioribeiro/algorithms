70231c2257950573f82a59a30b5501f1b0b97a3f
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2013-10-29 00:36:43 -0200

    add more tests and use decimal instead of float

--- geometria_computacional/point-in-polygon/point_in_polygon.py
+++ geometria_computacional/point-in-polygon/point_in_polygon.py
@@ -1,32 +1,36 @@
+from decimal import *
+getcontext().prec = 4
+
 class Point(object):
     def __init__(self, x, y):
-        self.x, self.y = x, y
+        self.x, self.y = Decimal(x), Decimal(y)
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
 class Segment(object):
     def __init__(self, p1, p2):
         self.p1, self.p2 = p1, p2
-    def __str__(self):
-        return "Segment[(%d, %d), (%d, %d)]" % (self.p1.x, self.p1.y, self.p2.x, self.p2.y)
+
+def drange(start, stop, step):
+    r = start
+    while r < stop:
+        yield Decimal(r)
+        r += Decimal(step)
 
 def inside(polygon, point):
     max_x = max([max(segment.p1.x, segment.p2.x) for segment in polygon]) + 10
     interceptions = 0
 
-    for x_coord in range(point.x, max_x):
-        for segment in polygon:
+    for segment in polygon:
+        for x_coord in drange(point.x, max_x, 0.01):
             p = Point(x_coord, point.y)
             if p == segment.p1 or p == segment.p2: return True
-            if point_in_segment(Point(x_coord, point.y), segment):
-                interceptions += 1
+            elif point_in_segment(p, segment): interceptions += 1
 
     return False if interceptions % 2 == 0 else True
 
 def point_in_segment(point, segment):
-    m = float(segment.p1.x - segment.p2.x)/(segment.p1.y - segment.p2.y)
+    m = Decimal(segment.p2.y - segment.p1.y)/(segment.p2.x - segment.p1.x)
     q = segment.p1.y - m * segment.p1.x
     return True if (point.y) == (m*point.x + q) else False
-
-
 
--- geometria_computacional/point-in-polygon/tests.py
+++ geometria_computacional/point-in-polygon/tests.py
@@ -4,12 +4,29 @@ def test_point_should_be_outside_polygon():
     p1, p2, p3 = Point(10, 10), Point(35, 20), Point(30,40)
     triangle = [Segment(p1, p2), Segment(p2, p3), Segment(p3, p1)]
 
-    point_1 = Point(10, 25)
-    assert not inside(triangle, point_1)
+    point = Point(10, 25)
+    assert not inside(triangle, point)
 
 def test_point_on_edge_should_be_inside_polygon():
     p1, p2, p3 = Point(10, 10), Point(35, 20), Point(30,40)
     triangle = [Segment(p1, p2), Segment(p2, p3), Segment(p3, p1)]
 
-    point_2 = Point(10, 10) #edge
-    assert inside(triangle, point_2)
+    point = Point(10, 10) #edge
+    assert inside(triangle, point)
+
+def test_point_on_inside_should_be_inside_polygon():
+    p1, p2, p3 = Point(10, 10), Point(35, 20), Point(30,40)
+    triangle = [Segment(p1, p2), Segment(p2, p3), Segment(p3, p1)]
+
+    point = Point(20, 20)
+    assert inside(triangle, point)
+
+    point = Point(30, 20)
+    assert inside(triangle, point)
+
+    point = Point(25, 25)
+    assert inside(triangle, point)
+
+
+
+
