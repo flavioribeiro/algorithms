371ddefaa7c85f84ad28da1c3e07c6518a33fe45
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2013-11-06 18:31:36 -0200

    update graham

--- geometria_computacional/hull-convex/graham.py
+++ geometria_computacional/hull-convex/graham.py
@@ -1,20 +1,17 @@
-TURN_LEFT = 1
-
 def turn(p, q, r):
     return cmp((q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1]), 0)
 
-def _keep_left(hull, r):
-    while len(hull) > 1 and turn(hull[-2], hull[-1], r) != TURN_LEFT:
+def keep_left(hull, r):
+    while len(hull) > 1 and turn(hull[-2], hull[-1], r) != 1:
             hull.pop()
     if not len(hull) or hull[-1] != r:
         hull.append(r)
     return hull
 
 def convex_hull(points):
-    """Returns points on convex hull of an array of points in CCW order."""
     points = sorted(points)
-    l = reduce(_keep_left, points, [])
-    u = reduce(_keep_left, reversed(points), [])
+    l = reduce(keep_left, points, [])
+    u = reduce(keep_left, reversed(points), [])
     return l.extend(u[i] for i in xrange(1, len(u) - 1)) or l
 
 # A = [18.95,26.475]
